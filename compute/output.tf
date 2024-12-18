cdfc18b95a6056b36d4ce988fa4b341bc4ca5915
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2024-12-18 19:38:12 +0530

    Create output.tf

---
+++ compute/output.tf
@@ -0,0 +1,15 @@
+output "public_instance_dns" {
+  value = aws_instance.public_instance.public_dns
+}
+
+output "vault_instance_dns" {
+  value = aws_instance.vault.private_dns
+}
+
+output "vault_instance_id" {
+  value = aws_instance.vault.id
+}
+
+output "consul_instances_dns" {
+  value = aws_instance.consul.*.private_dns
+}
