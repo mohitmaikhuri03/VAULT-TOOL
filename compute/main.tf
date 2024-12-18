981b8f052b9043e636e9d592112cee226be65ce4
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2024-12-18 22:42:06 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "public_instance" {
   instance_type          = var.instance_type
   subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [var.public_sg_id]
+  key_name               = var.key_name
   tags = {
     Name = "public-instance"
   }
